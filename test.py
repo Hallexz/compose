2f6b601375a612b5bff84df123e2e3926841008f
Hallex <136874674+Hallexz@users.noreply.github.com>
2024-05-09 14:51:15 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,24 @@
-import pytest
-from app import app
-from flask import json
-from unittest.mock import patch
-from pymongo import MongoClient
-from mongomock import MongoClient as MockedMongoClient
+import unittest
+from app import app 
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
 
-@patch('pymongo.MongoClient', new=MockedMongoClient)
-def test_get(client):
-    response = client.get('/', query_string={'key': 'key'})
-    assert response.status_code == 200
+class FlaskTestCase(unittest.TestCase):
 
-@patch('pymongo.MongoClient', new=MockedMongoClient)
-def test_post(client):
-    response = client.post('/', data=json.dumps({'key': 'key', 'value': 'value'}), content_type='application/json')
-    assert response.status_code == 200
-    assert response.get_data(as_text=True) == 'Inserted'
+    def setUp(self):
+        self.app = app
+        self.client = self.app.test_client()
 
-@patch('pymongo.MongoClient', new=MockedMongoClient)
-def test_put(client):
-    response = client.put('/', data=json.dumps({'key': 'key', 'value': 'new_value'}), content_type='application/json')
-    assert response.status_code == 200
-    assert response.get_data(as_text=True) == 'Updated'
+    def test_get_index(self):
+        response = self.client.get('/', query_string={'key': 'some_key'})
+        self.assertEqual(response.status_code, 200)
+    def test_post_index(self):
+        response = self.client.post('/', data={'key': 'some_key', 'value': 'some_value'})
+        self.assertEqual(response.status_code, 201) 
+
+    def test_put_index(self):
+        response = self.client.put('/', data={'key': 'some_key', 'value': 'new_value'})
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
